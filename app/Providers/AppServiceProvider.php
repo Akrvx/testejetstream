e3469e49d4118d771065184ac4f467d01357f010
Akrvx <mauriciolc.codes@gmail.com>
2025-12-10 12:17:55 -0300

    Forcar HTTPS

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // <--- ADICIONADO
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Se estiver em produção (Railway), força tudo para HTTPS
+        if($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
